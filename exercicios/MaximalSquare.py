a1e964977446feeaf932df5b7d4320e6d4f7162a
henriqtorresl <henriquetlandin@gmail.com>
2023-07-10 14:11:09 -0300

    adicionando a solução do exercicio1

--- exercicios/MaximalSquare.py
+++
@@ -1,1 +0,0 @@
-# Solucionar o exercicio
--- exercicios/Pascal'sTriangleII.py
+++ exercicios/Pascal'sTriangleII.py
@@ -1,1 +1,10 @@
-# Solucionar o exercicio
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+
+        linha = [1] * (rowIndex + 1)  
+
+        for i in range(2, rowIndex + 1):
+            for j in range(1, i):
+                linha[i - j] += linha[i - j - 1]  
+
+        return linha
